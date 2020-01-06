bebf1503616fa1e47afb683d597dd365e0194e36
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2020-01-06 16:31:50 -0500

    Building out global state

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+
+import * as reset from './styles/reset.css';
+import * as global from './styles/global.css';
+
+import { StateProvider } from './contexts/StateProvider';
+import { initialState } from './reducers/initialState';
+import { mainReducer } from './reducers/mainReducer';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    ${global}
+`;
+
+const theme = {}
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        <StateProvider initialState={initialState} reducer={mainReducer}>
+              <App />
+        </StateProvider>
+      </>
+    </ThemeProvider>,
+    document.getElementById('root')
+  );
+  
