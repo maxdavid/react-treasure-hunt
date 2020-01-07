91cbc5ee40c7ef21d5bb5d281d357fe5cff7fca5
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2020-01-07 13:53:31 -0500

    Mining working

--- src/utility/mining.js
+++ src/utility/mining.js
@@ -2,24 +2,25 @@ import { axiosWithAuth } from './axiosTypes';
 import { sha256 } from 'js-sha256';
 
 export const mining = async () => {
-  let count = 0;
-  while (true) {
-    console.log(count++);
-    let lastProof = await axiosWithAuth().get('bc/last_proof/');
-    let { difficulty, cooldown, proof } = lastProof.data;
-    let newProof = await proofOfWork(proof, difficulty);
-    let miningResult = await axiosWithAuth().post('bc/mine/', {
-      proof: newProof,
-    });
-    sleep(miningResult.data.cooldown);
-  }
+  let lastProof = await axiosWithAuth().get('bc/last_proof/');
+  let { difficulty, cooldown, proof } = lastProof.data;
+  let newProof = await proofOfWork(proof, difficulty);
+  let miningResult = await axiosWithAuth().post('bc/mine/', {
+    proof: newProof,
+  });
+  sleep(miningResult.data.cooldown);
 };
 
 const proofOfWork = (lastProof, difficulty) => {
   let guess =
     Math.random() * (Number.MAX_SAFE_INTEGER - Number.MIN_SAFE_INTEGER) +
     Number.MIN_SAFE_INTEGER;
+  let count = 0;
   while (!validProof(lastProof, guess, difficulty)) {
+    ++count;
+    if (count % 500000 === 0) {
+      console.log('still going!', count);
+    }
     guess =
       Math.random() * (Number.MAX_SAFE_INTEGER - Number.MIN_SAFE_INTEGER) +
       Number.MIN_SAFE_INTEGER;
